0d9dbbc66a5e192fa8671f9f411623778e50d5cb
Amin Abbasloo <abbasloo@cs.uni-bonn.de>
2019-03-08 16:26:36 +0100

    Add files via upload

--- vis/vis2.m
+++ vis/vis2.m
@@ -1,4 +1,6 @@
 
 d=load('/home/mabbasloo/Documents/carData3/data/lbo/f001_S2000001_1.mat');
+dd=load('/home/mabbasloo/Documents/carData3/data/geovec/f001_S2000001_1.mat');
 s=loadoff('/home/mabbasloo/Documents/carData3/f001_S2000001_1.off');
 figure; clf; show_shape(s, d.Phi(:, 15));
+figure; clf; show_shape(s, dd.desc(:, 15));
